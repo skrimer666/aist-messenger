c94bc994ef00d98f3e8ac2acac5978cd5dfd30e0
Tenper1 <99241957+Tenper1@users.noreply.github.com>
2026-01-29 23:39:50 +0300

    Create React App

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
