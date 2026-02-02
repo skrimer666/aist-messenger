00bd9de593abf1e7fbf667e9e3e1887c1bdcb077
skrimer666 <susojkindaniil867@gmail.com>
2026-02-02 21:29:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-// Позже добавишь ChatLayout
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/chat" element={<div>Чаты (в разработке)</div>} />
       </Routes>
     </Router>
   );
