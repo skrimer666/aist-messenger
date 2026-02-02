94aa256a9e904bb213e74027ba62c9c01b62b9e9
skrimer666 <susojkindaniil867@gmail.com>
2026-02-02 18:48:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
