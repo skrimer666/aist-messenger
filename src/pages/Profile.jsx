e123064c09ed4d334c49671758be60019a2238bf
skrimer666 <susojkindaniil867@gmail.com>
2026-02-02 20:41:01 +0300

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,27 +1,15 @@
-// src/pages/Profile.jsx
-import QRCode from 'qrcode.react';
-import { useEffect, useState } from 'react';
-
-export default function Profile() {
-  const [userId, setUserId] = useState('');
-
-  useEffect(() => {
-    setUserId(localStorage.getItem('userId') || '');
-  }, []);
-
-  return (
-    <div style={{ padding: 20, textAlign: 'center' }}>
-      <h1>Мой профиль</h1>
-      {userId ? (
-        <>
-          <p>ID: {userId}</p>
-          <QRCode value={`aist://user/${userId}`} size={200} />
-        </>
-      ) : (
-        <p>
-          Не авторизован. <a href="/">Войти</a>
-        </p>
-      )}
-    </div>
-  );
-}
+return (
+  <div style={{ padding: 20, textAlign: 'center' }}>
+    <h1>Мой профиль</h1>
+    {userId ? (
+      <>
+        <p>ID: {userId}</p>
+        <QRCode value={`aist://user/${userId}`} size={200} />
+      </>
+    ) : (
+      <p>
+        Не авторизован. <a href="/">Войти</a>
+      </p>
+    )}
+  </div>
+);
