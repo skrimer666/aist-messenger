5dc7b1335b453910590d22445ea215eea35e089c
skrimer666 <susojkindaniil867@gmail.com>
2026-02-05 18:54:40 +0300

    обнова

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
+// src/setupProxy.js (убедитесь, что находится в папке src/)
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://api.get-aist.ru', // Изменено на ваш бэкенд
+      target: 'http://45.150.10.220:3001', // Или ваш актуальный бэкенд
       changeOrigin: true,
-      secure: true,
     })
   );
 };
